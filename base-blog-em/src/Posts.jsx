3f29da87c57472f92026dbe4894b4f79eaa99e3c
skis630 <skis630@aucklanduni.ac.nz>
2022-08-26 16:15:35 +0300

    Fetch posts using useQuery

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -16,9 +16,10 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const {data, isLoading, isError} = useQuery("posts", fetchPosts);
+  const {data, isLoading, isError} = useQuery("posts", fetchPosts, {staleTime: 2000});
 
   if (isLoading) return <h3>Loading...</h3>;
+  if (isError) return <h3>Oops, something went wrong...</h3>;
 
   return (
     <>
