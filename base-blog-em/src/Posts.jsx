43aec705c29307822357056138a85e62e692fdd2
skis630 <skis630@aucklanduni.ac.nz>
2022-08-27 23:14:18 +0300

    Prefetch next page posts

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import {useQuery} from "react-query";
+import { useEffect, useState } from "react";
+import {useQuery, useQueryClient} from "react-query";
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
@@ -14,9 +14,18 @@ async function fetchPosts(pageNum) {
 export function Posts() {
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery(["posts", nextPage], () => fetchPosts(nextPage));
+    }
+  }, [currentPage, queryClient])
 
   // replace with useQuery
-  const {data, isLoading, isError} = useQuery(["posts", currentPage], () => fetchPosts(currentPage), {staleTime: 2000});
+  const {data, isLoading, isError} = useQuery(["posts", currentPage], () => fetchPosts(currentPage),
+    {staleTime: 2000, keepPreviousData: true});
 
   if (isLoading) return <h3>Loading...</h3>;
   if (isError) return <h3>Oops, something went wrong...</h3>;
