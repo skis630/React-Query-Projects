466fd385003d825c2bc961bb7adb5144e5157e02
skis31 <sara@classiq.io>
2022-09-13 15:40:29 +0300

    Config default re-fetch options for useQuery

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -12,20 +12,11 @@ async function getTreatments(): Promise<Treatment[]> {
 
 export function useTreatments(): Treatment[] {
   const fallback = [];
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
-    staleTime: 600000, // 10 mins
-    cacheTime: 900000, // 15 mins (doesn't make sense that staleTime will exceed cacheTime)
-    refetchOnMount: false,
-    refetchOnReconnect: false,
-    refetchOnWindowFocus: false,
-  });
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
   return data;
 }
 
 export function usePrefetchTreatments(): void {
   const queryClient = useQueryClient();
-  queryClient.prefetchQuery(queryKeys.treatments, getTreatments, {
-    staleTime: 600000, // 10 mins
-    cacheTime: 900000, // 15 mins
-  });
+  queryClient.prefetchQuery(queryKeys.treatments, getTreatments);
 }
