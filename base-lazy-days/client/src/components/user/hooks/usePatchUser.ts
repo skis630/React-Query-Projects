bc9bf896ec1eecfd209d50080a34c91457996198
skis630 <skis630@aucklanduni.ac.nz>
2022-09-25 16:38:32 +0300

    Implement optimistic update for user data

--- base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
+++ base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
@@ -1,5 +1,6 @@
 import jsonpatch from 'fast-json-patch';
-import { UseMutateFunction, useMutation } from 'react-query';
+import { UseMutateFunction, useMutation, useQueryClient } from 'react-query';
+import { queryKeys } from 'react-query/constants';
 
 import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
@@ -33,18 +34,46 @@ export function usePatchUser(): UseMutateFunction<
 > {
   const { user, updateUser } = useUser();
   const toast = useCustomToast();
+  const queryClient = useQueryClient();
 
   const { mutate: patchUser } = useMutation(
     (newUser: User | null) => patchUserOnServer(newUser, user),
     {
+      // onMutate returns context that is passed to onError
+      onMutate: async (newData: User | null) => {
+        // cancel any outgoing queries for user data, so old server data doesn't overwrite our optimisitic update
+        queryClient.cancelQueries(queryKeys.user);
+
+        // snapshot of previous user value
+        const previousUserData: User = queryClient.getQueryData(queryKeys.user);
+
+        // optimistically update the cache with new user value
+        updateUser(newData);
+
+        // return context object with snapshotted value
+        return { previousUserData };
+      },
+      onError: (error, newData, context) => {
+        // roll back cache to saved value
+        if (context.previousUserData) {
+          updateUser(context.previousUserData);
+          toast({
+            title: 'Update failed; restoring previous values',
+            status: 'warning',
+          });
+        }
+      },
       onSuccess: (newUser: User | null) => {
         if (newUser) {
-          updateUser(newUser);
           toast({
             title: 'User updated!',
             status: 'success',
           });
         }
+      },
+      onSettled: () => {
+        // invalidate user query to make sure we're in sync with server data
+        queryClient.invalidateQueries(queryKeys.user);
       },
     },
   );
