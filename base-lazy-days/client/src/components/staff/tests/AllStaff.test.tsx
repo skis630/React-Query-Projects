ef48eb81f951e76bc48df1e0ef861ae78a592bfe
skis630 <skis630@aucklanduni.ac.nz>
2022-10-13 15:14:04 +0300

    Code quiz: test staff query data handled properly

--- base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
@@ -3,11 +3,16 @@ import { screen } from '@testing-library/react';
 // import { rest } from 'msw';
 // import { defaultQueryClientOptions } from '../../../react-query/queryClient';
 // import { server } from '../../../mocks/server';
-// import { renderWithClient } from '../../../test-utils';
+import { renderWithQueryClient } from '../../../test-utils';
 import { AllStaff } from '../AllStaff';
 
-test('renders response from query', () => {
-  // write test here
+test('renders response from query', async () => {
+  renderWithQueryClient(<AllStaff />);
+
+  const staffTitles = await screen.findAllByRole('heading', {
+    name: /sandra|divya|mateo|michael/i,
+  });
+  expect(staffTitles).toHaveLength(4);
 });
 
 test('handles query error', async () => {
