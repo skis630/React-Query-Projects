d8d5325f4b3af25ccf318a1e8bc7216e90dc0774
skis630 <skis630@aucklanduni.ac.nz>
2022-10-14 15:49:05 +0300

    Mock logged-in user for tests

--- base-lazy-days/client/src/components/appointments/tests/Calendar.test.tsx
+++ base-lazy-days/client/src/components/appointments/tests/Calendar.test.tsx
@@ -1,16 +1,17 @@
 import { screen } from '@testing-library/react';
 import { rest } from 'msw';
 
+import { mockUser } from '../../../mocks/mockData';
 import { server } from '../../../mocks/server';
 // import { defaultQueryClientOptions } from '../../../react-query/queryClient';
 import { renderWithQueryClient } from '../../../test-utils';
 import { Calendar } from '../Calendar';
 
 // mocking useUser to mimic a logged-in user
-// jest.mock('../../user/hooks/useUser', () => ({
-//   __esModule: true,
-//   useUser: () => ({ user: mockUser }),
-// }));
+jest.mock('../../user/hooks/useUser', () => ({
+  __esModule: true,
+  useUser: () => ({ user: mockUser }),
+}));
 
 test('Reserve appointment error', async () => {
   // (re)set handler to return a 500 error for appointments
