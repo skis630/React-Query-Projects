a30a11bc55250bb0cf5337ae33ebf6a59933186d
skis630 <skis630@aucklanduni.ac.nz>
2022-10-14 15:32:36 +0300

    Code quiz: test useStaff filter functionality

--- base-lazy-days/client/src/components/staff/tests/useStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/useStaff.test.tsx
@@ -1,5 +1,19 @@
+import { act, renderHook } from '@testing-library/react-hooks';
+
+import { createQueryClientWrapper } from '../../../test-utils';
 import { useStaff } from '../hooks/useStaff';
 
 test('filter staff', async () => {
-  // the magic happens here
+  const { result, waitFor } = renderHook(useStaff, {
+    wrapper: createQueryClientWrapper(),
+  });
+
+  // wait for staff to populate
+  await waitFor(() => result.current.staff.length > 0);
+
+  // filter by facial treatment
+  act(() => result.current.setFilter('facial'));
+
+  // wait for staff to be filtered
+  await waitFor(() => result.current.staff.length === 3);
 });
